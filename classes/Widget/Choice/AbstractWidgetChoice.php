e23ff2dc79a0461ef21b45e35a0f18d5bd82f09e
Thomas-DALLA-FRANCESCA <thomas.dalla-francesca@orange.fr>
2015-06-17 11:43:24 +0200

    SAY NO TO FUCKERS

--- classes/Widget/Choice/AbstractWidgetChoice.php
+++ classes/Widget/Choice/AbstractWidgetChoice.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace ItechSup\FormFwk\Widget\Choice;
+namespace Itechsup\FormFwk\Widget\Choice;
 
 use Itechsup\FormFwk\Widget\Widget;
+
 /**
  * AbstractWidgetChoice offers high level utilities for handing choices in web 
  * form. Awesome!
@@ -11,12 +12,19 @@ use Itechsup\FormFwk\Widget\Widget;
  */
 abstract class AbstractWidgetChoice extends Widget
 {
+
     protected $options = [];
-   
+
     public function __construct($name, $label = null, $htmlAttributes = [], $options = [])
     {
         parent::__construct($name, $label, $htmlAttributes);
         $this->options = $options;
     }
-    
+
+    protected function isOptionSelected($key)
+    {
+        return array_search($key, $this->data);
+        
+    }
+
 }
