f4e0968fbc4f9ba60b0bf0f34f2f0fb74e419fce
tomphi <thomas.philippe5555@gmail.com>
2015-06-17 13:55:12 +0200

    index (commit older version)

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ spl_autoload_register(function ($class) {
 $form = new Itechsup\FormFwk\Form\Form();
 $form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetText('txt_name', 'gruesome label for text'));
 $form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetMail('mail_name', 'gruesome label for mail'));
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\SimpleExpended('radio_name', 'gruesome label for radio',
+        [], ['toto', 'titi', 'tata']));
 
 if (!empty($_POST)) {
     $form->bind($_POST);
