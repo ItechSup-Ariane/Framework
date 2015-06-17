f30c5a7868bff980e79350bc35ddfc2643e76b26
Mcouteau <couteau.m@laposte.net>
2015-06-17 13:54:19 +0200

    Merge origin/master into MCU
    Conflicts:
    	classes/Widget/Choice/AbstractWidgetChoice.php
    	index.php

--- classes/Widget/Choice/AbstractWidgetChoice.php
+++ classes/Widget/Choice/AbstractWidgetChoice.php
@@ -1,36 +1,3 @@
-
-
-<?php
-
-namespace Itechsup\FormFwk\Widget\Choice;
-
-use Itechsup\FormFwk\Widget\Widget;
-
-/**
- * AbstractWidgetChoice offers high level utilities for handing choices in web 
- * form. Awesome!
- *
- * @author Corentin
- */
-abstract class AbstractWidgetChoice extends Widget
-{
-
-    protected $options = [];
-
-    public function __construct($name, $label = null, $htmlAttributes = [], $options = [])
-    {
-        parent::__construct($name, $label, $htmlAttributes);
-        $this->options = $options;
-    }
-
-    protected function isOptionSelected($key)
-    {
-        return $this->data !== null && array_search($key, $this->data) ;
-        
-    }
-
-}
-=======
 <?php
 
 namespace Itechsup\FormFwk\Widget\Choice;
