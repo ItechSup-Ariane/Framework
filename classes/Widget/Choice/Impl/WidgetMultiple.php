f645c7bb4473d6116d6211ca9f9e205b7942cea2
Mcouteau <couteau.m@laposte.net>
2015-06-17 13:56:57 +0200

    Merge origin/ALC into MCU
    Conflicts:
    	classes/Widget/Choice/Impl/WidgetMultiple.php

--- classes/Widget/Choice/Impl/WidgetMultiple.php
+++ classes/Widget/Choice/Impl/WidgetMultiple.php
@@ -1,18 +1,12 @@
-
 <?php
-
 namespace Itechsup\FormFwk\Widget\Choice\Impl;
-
-use Itechsup\FormFwk\Widget\WidgetChoice;
-
+use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoice;
 /**
  * Nice OO interface for our Form mail Widgets.
  */
-class WidgetMultiple extends WidgetChoice
+class WidgetMultiple extends AbstractWidgetChoice
 {
-
     protected $type = 'List';
-
     /**
      * Renderer HTML code for WidgetMultiple (HTML select multiple)
      * @return string
@@ -22,51 +16,46 @@ class WidgetMultiple extends WidgetChoice
         if ($this->data !== null) {
             $this->htmlAttributes['value'] = $this->data;
         }
-
         // And to be sure to generate a proper html text input with the proper name...
         $this->htmlAttributes['name'] = $this->name;
         $this->htmlAttributes['id'] = $this->getId();
-
         $output = $this->renderlabel();
         $output .= '<select ';
         $output .= $this->renderHtmlAttributes() . "Multiple";
         $output .= '/>';
         $output .= $this->renderOptions();
         $output .= '</select>';
-
         return $output;
     }
-
     /**
      * Renderer HTML Code for options and groups of select
      * @return string
      */
     public function renderOptions()
     {
-        foreach ($this->option as $key => $value) {
-            if (is_array($value)) {
-                $output .= '<optgroup label="'.$key.'">';
-                foreach ($value as $v) {
-                    $output .= renderOption($v);
-                }
-                $output .= '</optgroup>';
-            }
-            $output .= renderOption($key);
+        $output='';
+        foreach ($this->options as $key => $value) {
+//            if (is_array($value)) {
+//                $output .= '<optgroup label="'.$key.'">';
+//                foreach ($value as $k=>$v) {
+//                    $output .= renderOption($k, $v);
+//                }
+//                $output .= '</optgroup>';
+//            }
+            $output .= $this->renderOption($key, $value);
         }
         return $output;
     }
-
     /**
      * Renderer HTML Code for option of select
      * @param string $option
      * @return string
      */
-    public function renderOption($option)
+    public function renderOption($key, $value)
     {
-        if (in_array($option, $this->data)) {
-            return '<option selected>' . $option . '</option>';
+        if ($this->isOptionSelected($key)) {
+            return '<option value="'.$key.'" selected>' . $value . '</option>';
         }
-        return '<option>' . $option . '</option>';
+        return '<option value="'.$key.'">'.$value.'</option>';
     }
-
 }
