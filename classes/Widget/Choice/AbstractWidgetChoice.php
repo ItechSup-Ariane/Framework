a06a63a0ab5d328cd4252ccd53e6a5da13547ebb
CorentinChn <champ.corentin@hotmal.fr>
2015-06-17 16:22:11 +0200

    doc construct isOptionSelected

--- classes/Widget/Choice/AbstractWidgetChoice.php
+++ classes/Widget/Choice/AbstractWidgetChoice.php
@@ -14,13 +14,23 @@ abstract class AbstractWidgetChoice extends Widget
 {
 
     protected $options = [];
-
+    /**
+     * 
+     * @param string $name
+     * @param string $label
+     * @param array $htmlAttributes
+     * @param array $options
+     */
     public function __construct($name, $label = null, $htmlAttributes = [], $options = [])
     {
         parent::__construct($name, $label, $htmlAttributes);
         $this->options = $options;
     }
-
+    /**
+     * isOptionSelected return a boolean if the the bind $this->data is in the array $option[$key]
+     * @param string $key
+     * @return boolean
+     */
     protected function isOptionSelected($key)
     {
         return $this->data !== null && false !== array_search($key, $this->data);
