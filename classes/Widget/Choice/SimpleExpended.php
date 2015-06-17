283f14a74e5a4f486fda79df6fa99297458e9c25
tomphi <thomas.philippe5555@gmail.com>
2015-06-17 11:26:50 +0200

    commit Class SimpleExtended and Class WidgetRadio

---
+++ classes/Widget/Choice/SimpleExpended.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Itechsup\FormFwk\Widget\Choice;
+
+use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoice;
+use Itechsup\FormFwk\Widget\WidgetImpl\WidgetRadio;
+
+/**
+ * Description of WidgetRadio
+ *
+ * @author Thomas
+ */
+class SimpleExpended extends AbstractWidgetChoice
+{
+    
+    public function render()
+    {
+        $output = $this->renderLabel();
+        foreach($this->options as $key => $value){
+            $widgetRadio = new WidgetRadio($this->name, $value, ['id' => $this->getId().$key, 'value' => $key]);
+            $output .= $widgetRadio->render();
+        }
+        return $output;
+    }
+
+}
