c880648cb139c49ab1e5fd079795ace97120e45d
lepamplemousse <thomas@lepamplemousse.fr>
2015-06-17 15:11:35 +0200

    Modif Checkbox
    Code opérationnel, inspiré de celui de PFE et TPE

--- classes/Widget/Choice/Impl/WidgetCheckbox.php
+++ classes/Widget/Choice/Impl/WidgetCheckbox.php
@@ -2,38 +2,37 @@
 
 namespace Itechsup\FormFwk\Widget\Choice\Impl;
 
+use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoice;
 use Itechsup\FormFwk\Widget\Choice\Impl\WidgetCheckboxSelect;
 
-/*
-<input type="checkbox" name="choix1" value="1" /> label 1
-<input type="checkbox" name="choix2" value="2" /> label 2
-<input type="checkbox" name="choix3" value="3" /> label 3
-<input type="checkbox" name="choix4" value="4" /> label 4
-*/
-
-/**
- * Nice OO interface for our Form text Widgets.
- */
-class WidgetCheckbox extends WidgetCheckboxSelect
+class WidgetCheckbox extends AbstractWidgetChoice
 {
-    protected $type = 'checkbox';
     
-    /**
-     * Renderer for a list of checkbox widget
-     */
     public function render()
-    {               
-        $output = $this->renderlabel();
-        
-        $checkboxKey = 0;
-        $checkboxId = $this->getId();
-        
-        // Loop for each options
-        foreach($this->options as $checkboxValue => $checkboxLabel) {
-            $output .= $this->renderCheckbox($checkboxKey, $checkboxId, $checkboxValue, $checkboxLabel);
-            $checkboxKey++;
+    {
+        $output = $this->renderLabel();
+		
+		$i=0;
+		
+        foreach($this->options as $valueCheckbox => $labelCheckbox){
+			
+			
+            $arrayAttributes = ['id' => $this->getId().'_'.$valueCheckbox, 'value' => $valueCheckbox];
+			
+            if($this->isOptionSelected($valueCheckbox)){
+                $arrayAttributes['checked'] = "";
+            }
+			
+            $widgetCheckbox = new WidgetCheckboxSelect($this->name.'['.$i.']', $labelCheckbox, $arrayAttributes);
+			
+            $output .= str_replace('for', 'class = "checkbox" for',$widgetCheckbox->render());
+			
+			$widgetCheckbox = null;
+			
+			$i++;
+			
         }
-
+		
         return $output;
     }
 }
--- classes/Widget/Choice/Impl/WidgetCheckboxSelect.php
+++ classes/Widget/Choice/Impl/WidgetCheckboxSelect.php
@@ -2,30 +2,11 @@
 
 namespace Itechsup\FormFwk\Widget\Choice\Impl;
 
-use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoice;
+use Itechsup\FormFwk\Widget\WidgetInput;
 
-abstract class WidgetCheckboxSelect extends AbstractWidgetChoice
+class WidgetCheckboxSelect extends WidgetInput
 {
-    /**
-     * Renderer for a checkbox widget
-     */
-    public function renderCheckbox($checkboxKey, $checkboxId, $checkboxValue, $checkboxLabel)
-    {
-
-        // And to be sure to generate a proper html text input with the proper name...
-        $this->htmlAttributes['type'] = $this->type;
-        $this->htmlAttributes['name'] = $this->name.'_'.$checkboxKey;
-        $this->htmlAttributes['id'] = $checkboxId.'_'.$checkboxKey;
-        $this->htmlAttributes['value'] = $checkboxValue;
-        
-        $output = '<input ';
-        $output .= $this->renderHtmlAttributes();
-        if ($this->isOptionSelected($this->htmlAttributes['name'])) {
-            $output .= 'checked ';
-        }
-        $output .= '/>';
-        $output .= $checkboxLabel;
-
-        return $output;
-    }
+    
+    protected $type = "checkbox";
+    
 }
