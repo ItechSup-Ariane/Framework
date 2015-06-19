5421a5f90221a0ccee3bbbb1cbedd994db2b0578
Mcouteau <couteau.m@laposte.net>
2015-06-19 09:41:59 +0200

    merge on ALCMCU

--- index.php
+++ index.php
@@ -21,8 +21,7 @@ spl_autoload_register(function ($class) {
 $form = new Itechsup\FormFwk\Form\Form();
 $form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetText('txt_name', 'gruesome label for text'));
 $form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetMail('mail_name', 'gruesome label for mail'));
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetMultiple('list_multiple', 'My big list', array("name"=>"maliste"),array("Group1"=>array("1"=>"1","2"=>"2","3"=>"3","4"=>"4"),"Group2"=>array("5"=>"5","6"=>"6","7"=>"7","8"=>"8"))));
-
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetMultiple('list_multiple', 'My big list', array("name"=>"maliste"),array("Group1"=>array("1"=>"1","2"=>"2","3"=>"3","4"=>"4"),"Group2"=>array("5"=>"5","6"=>"6","7"=>"7","8"=>"8"),"9"=>"9","10"=>"10","11"=>"11")));
 
 $option[] = "1"; // $option = ["1", "2", "3"] <-- suggestion de Corentin que je trouve fort à propos
 $option[] = "2";
@@ -31,11 +30,11 @@ $option[] = "3";
 $html[] = "";
 
 $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSelectSimple('liste', 'gruesome label for mail',$html,$option));
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSimpleExpanded('radio_name', 'gruesome label for radio', [], ['toto', 'titi', 'tata']));
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\SimpleExpended('radio_name', 'gruesome label for radio', [], ['toto', 'titi', 'tata']));
 
 $options = array('a' => 'Ma lettre A', 'b' => 'Ma lettre B', 'c' => 'Ma lettre C');
 $htmlAttributes = array();
-$form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetCheckbox('checkbox_name', 'label for CB', $htmlAttributes, $options));
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetCheckbox('checkbox_name', 'label for CB', $htmlAttributes, $options));
 
 if (!empty($_POST)) {
     $form->bind($_POST);
@@ -57,3 +56,4 @@ if (!empty($_POST)) {
         </div>
 	</body>
 </html>
+
