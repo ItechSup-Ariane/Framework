88242ad060ca1e08356dc8dbd20737bc8999d0c3
tomphi <thomas.philippe5555@gmail.com>
2015-06-19 11:22:05 +0200

    commit expanded widgets finished + new AbstractWidgetChoiceExpanded class created

--- classes/Widget/Choice/Impl/WidgetMultipleExpanded.php
+++ classes/Widget/Choice/Impl/WidgetMultipleExpanded.php
@@ -2,43 +2,13 @@
 
 namespace Itechsup\FormFwk\Widget\Choice\Impl;
 
-use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoice;
+use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoiceExpanded;
 use Itechsup\FormFwk\Widget\WidgetImpl\WidgetCheckbox;
 
-class WidgetMultipleExpanded extends AbstractWidgetChoice
-{
+class WidgetMultipleExpanded extends AbstractWidgetChoiceExpanded
+{	
 
-    public function render()
-    {
-
-        $output = $this->renderLabel();
-
-        foreach ($this->options as $valueCheckbox => $labelCheckbox){
-
-            // Array case
-            if (is_array($labelCheckbox)) {
-
-                // Redefine value for Group
-                $legendGroupCheckbox = $valueCheckbox;
-                $arrayGroupCheckbox = $labelCheckbox;
-
-                // Generate HTML for Group
-                $output .= '<fieldset><legend>'.$legendGroupCheckbox.'</legend>';
-                foreach($arrayGroupCheckbox as $valueGroup => $labelGroup) {
-                    $output .= $this->renderOptions($valueGroup, $labelGroup);
-                }
-                $output .= '</fieldset>';
-
-            } else {
-                $output .= $this->renderOptions($valueCheckbox, $labelCheckbox);
-            }
-        }
-
-        return $output;
-
-    }	
-
-    private function renderOptions($value, $label)
+    protected function renderOptions($value, $label)
     {
 
         $output = '';
--- classes/Widget/Choice/Impl/WidgetSimpleExpanded.php
+++ classes/Widget/Choice/Impl/WidgetSimpleExpanded.php
@@ -8,45 +8,18 @@
 
 namespace Itechsup\FormFwk\Widget\Choice\Impl;
 
-use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoice;
+use Itechsup\FormFwk\Widget\Choice\AbstractWidgetChoiceExpanded;
 use Itechsup\FormFwk\Widget\WidgetImpl\WidgetRadio;
 
 /**
  * Class WidgetSimpleExpended
  * contains a group of radioboxes with a simple selection
  */
-class WidgetSimpleExpanded extends AbstractWidgetChoice
-{
+class WidgetSimpleExpanded extends AbstractWidgetChoiceExpanded
+{   
     
-    /**
-     * render function
-     * @return string containing the html code to generate the group of radioboxes
-     */
-    public function render()
-    {
-        $output = $this->renderLabel();
-        foreach($this->options as $valueRadio => $labelRadio){
-            // Array case
-            if(is_array($labelRadio)){
-                // Redefine value for Group
-                $legendGroupRadio = $valueRadio;
-                $arrayGroupRadio = $labelRadio;
-                // Generate HTML for Group
-                $output .= '<fieldset><legend>'.$legendGroupRadio.'</legend>';
-                foreach($arrayGroupRadio as $valueGroup => $labelGroup){
-                    $output .= $this->renderOptions($valueGroup, $labelGroup);
-                }
-                $output .= '</fieldset>';
-            } else {
-                $output .= $this->renderOptions($valueRadio, $labelRadio);
-            }
-        }
-        return $output;
-    }
-    
-    private function renderOptions($value, $label){
+    protected function renderOptions($value, $label){
         $arrayAttributes = ['id' => $this->getId().'_'.$value, 'value' => $value];
-        var_dump($value);
         if($this->isOptionSelected($value)){
             $arrayAttributes['checked'] = "";
         }
