2924daf0e23b86c7000743d7fdd8dd4cc7cec0a4
jf-jung <pedagogie@itechsup.fr>
2015-06-19 11:36:00 +0200

    merging...

--- index.php
+++ index.php
@@ -19,22 +19,14 @@ spl_autoload_register(function ($class) {
 });
 
 $form = new Itechsup\FormFwk\Form\Form();
-$form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetText('txt_name', 'gruesome label for text'));
-$form->addWidget(new Itechsup\FormFwk\Widget\WidgetImpl\WidgetMail('mail_name', 'gruesome label for mail'));
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetMultiple('list_multiple', 'My big list', array("name"=>"maliste"),array("Group1"=>array("1"=>"1","2"=>"2","3"=>"3","4"=>"4"),"Group2"=>array("5"=>"5","6"=>"6","7"=>"7","8"=>"8"),"9"=>"9","10"=>"10","11"=>"11")));
+$html = [];
+$options = ['lettres' => ['a' => 'Ma lettre A', 'b' => 'Ma lettre B', 'c' => 'Ma lettre C'], '001' => 'Mr Toto'];
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetMultiple('list_multiple', 'My big list', $html, $options));
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSelectSimple('liste', 'gruesome label for select', $html, $options));
+// $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\SimpleExpended('radio_name', 'gruesome label for radio', [], ['toto', 'titi', 'tata']));
 
-$option[] = "1"; // $option = ["1", "2", "3"] <-- suggestion de Corentin que je trouve fort à propos
-$option[] = "2";
-$option[] = "3";
-
-$html[] = "";
-
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSelectSimple('liste', 'gruesome label for mail',$html,$option));
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\SimpleExpended('radio_name', 'gruesome label for radio', [], ['toto', 'titi', 'tata']));
-
-$options = array('a' => 'Ma lettre A', 'b' => 'Ma lettre B', 'c' => 'Ma lettre C');
-$htmlAttributes = array();
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetCheckbox('checkbox_name', 'label for CB', $htmlAttributes, $options));
+// $htmlAttributes = array();
+// $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetCheckbox('checkbox_name', 'label for CB', $htmlAttributes, $options));
 
 if (!empty($_POST)) {
     $form->bind($_POST);
