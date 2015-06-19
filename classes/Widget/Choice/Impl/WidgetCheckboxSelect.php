a1ebedad8d31578f3f4e920a641cc1d3474da934
Mcouteau <couteau.m@laposte.net>
2015-06-19 11:10:05 +0200

    Recup Structure Master

---
+++ classes/Widget/Choice/Impl/WidgetCheckboxSelect.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Itechsup\FormFwk\Widget\Choice\Impl;
+
+use Itechsup\FormFwk\Widget\WidgetInput;
+
+class WidgetCheckboxSelect extends WidgetInput
+{
+    
+    protected $type = "checkbox";
+    
+}
