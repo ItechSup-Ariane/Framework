c13940f6efd064fdea6b78f37123724a8882cdb0
Thomas-DALLA-FRANCESCA <thomas.dalla-francesca@orange.fr>
2015-06-19 09:10:09 +0200

    Gestion des OptGroup

--- classes/Widget/Choice/Impl/WidgetSelectSimple.php
+++ classes/Widget/Choice/Impl/WidgetSelectSimple.php
@@ -18,20 +18,45 @@ class WidgetSelectSimple extends AbstractWidgetChoice
 
     /*
     * Render function : Method to render the widget
+    * 
+    * @return string : the HTML string for display
     */
     public function render()
     {
         $this->options;
-        $retour = $this->renderlabel().'<select name="'.$this->name.'[]">';
+        $return = $this->renderlabel().'<select name="'.$this->name.'[]">';
         foreach ($this->options as $key => $value) {
-            $retour .= '<option value="'.$value.'"';
-            if ($this->isOptionSelected($value)) {
-                $retour .= ' selected';
+            if (is_array($value)){
+                $return .= '<optgroup label="'.$key.'">';
+                foreach ($value as $opt => $lbl) {
+                    $return .= $this->renderOptions($opt, $lbl);
+                }
+                $return .= '</optgroup>';
+                
+            } else {
+                $return .= $this->renderOptions($key, $value);
             }
-            $retour .= '>'.$value.'</option>';
         }
-        $retour .= '</select>';
+        $return .= '</select>';
         
-        return $retour;
+        return $return;
+    }
+    
+    /**
+     * Render Options : Method to render the options of the select
+     * 
+     * @param type $key : the key of the option to render
+     * @param type $value : the value of the option to render
+     * @return string : the HTML string for display
+     */
+    private function renderOptions($key, $value)
+    {
+        $return = '<option value="'.$key.'"';
+        if ($this->isOptionSelected($key)) {
+            $return .= ' selected';
+        }
+        $return .= '>'.$value.'</option>'; 
+        
+        return $return;
     }
 }
