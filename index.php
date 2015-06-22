1b470cde404fcd311bb9cf3ee1fe187e2f8d47fc
lepamplemousse <thomas@lepamplemousse.fr>
2015-06-22 14:52:46 +0200

    Pb de fin de lignes

--- index.php
+++ index.php
@@ -20,12 +20,15 @@ spl_autoload_register(function ($class) {
 
 $form = new Itechsup\FormFwk\Form\Form();
 $options = ['les lettres' => ['a' => 'Ma lettre A', 'b' => 'Ma lettre B', 'c' => 'Ma lettre C'], '001' => 'tutu is so plop'];
-$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSelectSimple('s', 'label1', [], $options), [new \Itechsup\FormFwk\Validator\ValidatorYouShouldNotPass()]);
+// $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSelectSimple('s', 'label1', [], $options), [new \Itechsup\FormFwk\Validator\ValidatorYouShouldNotPass()]);
+$form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSelectSimple('s', 'label1', [], $options));
 $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetSimpleExpanded('se', 'label2', [], $options));
 $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetMultipleExpanded('me', 'label3', [], $options));
 $form->addWidget(new Itechsup\FormFwk\Widget\Choice\Impl\WidgetMultiple('m', 'label4', [], $options));
 $form->addGroupValidator("groupe1", [new Itechsup\FormFwk\Validator\ValidatorComparisonImpl\ValidatorEqual("erreur")]);
 $form->bindGroupValidator("groupe1", ["s", "se"]);
+$form->addGroupValidator("groupe2", [new Itechsup\FormFwk\Validator\ValidatorComparisonImpl\ValidatorEqual("pouet")]);
+$form->bindGroupValidator("groupe2", ["se", "me"]);
 
 if (!empty($_POST)) {
     $form->bind($_POST);
