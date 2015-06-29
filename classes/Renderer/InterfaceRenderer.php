3e1a66898fb794b474293d83efa34a11bed8225b
tomphi <thomas.philippe5555@gmail.com>
2015-06-29 14:21:17 +0200

    same commit without error

---
+++ classes/Renderer/InterfaceRenderer.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Itechsup\FormFwk\Renderer;
+
+/**
+ * Description of RenderFactory
+ *
+ * @author Thomas
+ */
+class InterfaceRenderer
+{
+
+    public function getHtmlDeb();
+
+    public function getHtmlFin();
+    
+}
