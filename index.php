b2944adb0fa7fdc6e668cf7629fc22a44b766ee3
Thomas-DALLA-FRANCESCA <thomas.dalla-francesca@orange.fr>
2015-07-06 09:05:00 +0200

    Modification du type de rendu du formulaire

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if (!empty($_POST)) {
     <body>
         <div id="wrapper">
             <?php 
-                echo $form->render('tasgfswdble'); 
+                echo $form->render('table'); 
             
                 if (!empty($_POST)) {
                     echo '<pre>';
